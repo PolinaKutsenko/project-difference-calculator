07e748d6f53da9c0a0024f2c40dffe618e502cae
Polina Kutsenko <polly153@mail.ru>
2022-08-14 22:13:42 +0300

    add changes in main files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This project is created by Polina Kutsenko.
+
+https://asciinema.org/a/tLkSJH4En9AgltQRjKcBcf1jx
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import genDiff from '../src/index.js'
 import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
@@ -7,7 +7,11 @@ const program = new Command();
 program
 
   .version('0.0.1', '-V, --version', 'output the version number')
-
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();

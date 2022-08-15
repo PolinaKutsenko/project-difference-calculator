ecdd8888ae8fe76b27090732306819a931a8383d
Polina Kutsenko <polly153@mail.ru>
2022-08-15 21:54:10 +0300

    add eslint, tests and workflows

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('genDiff .json', () => {
+  const result = readFile('result.txt');
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
+});
